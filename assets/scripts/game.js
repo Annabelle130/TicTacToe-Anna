70a313808690f655d99b4a302b0e686bbff763f0
AnnaShalaginova <anya.shalaginova@gmail.com>
2019-05-31 12:19:55 -0400

    Updated GetGames

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -18,7 +18,7 @@ const checkForWinner = function () {
     (gameArray[0] === 'x' && gameArray[4] === 'x' && gameArray[8] === 'x') ||
     (gameArray[2] === 'x' && gameArray[4] === 'x' && gameArray[6] === 'x')) {
     store.game.over = true
-    $('#game-status').html('Player x won!')
+    $('#create-game-message').html('Player x won!')
   } else if (
     (gameArray[0] === 'o' && gameArray[1] === 'o' && gameArray[2] === 'o') ||
     (gameArray[3] === 'o' && gameArray[4] === 'o' && gameArray[5] === 'o') ||
@@ -33,7 +33,7 @@ const checkForWinner = function () {
   } else if (store.game.cells.every(checkForDraw)) {
     store.game.over = true
     $('#create-game-message').html('It\'s a draw!')
-    //game-status
+    // game-status
   }
 }
 
@@ -50,20 +50,20 @@ const playGame = function (event) {
       $(event.target).text('o')
       store.game.cells[boardIndex] = 'o'
       whoseturn = 'x'
-      checkForWinner()
+    }
+    checkForWinner()
     api.updateGame(boardIndex, store.game.cells[boardIndex], store.game.over)
       .then(ui.onUpdateSuccess)
       .catch(ui.onUpdateFailure)
   }
 }
 
-  // objGame.game.
-  // cell.index = 0
-  // objGame.game.cell.value = 'x'
-  // $('.cell').on('click', playGame)
-  // if (whoseturn !== 0) {
-  //   $('#diff-cell').text('Choose a different cell')
-
+// objGame.game.
+// cell.index = 0
+// objGame.game.cell.value = 'x'
+// $('.cell').on('click', playGame)
+// if (whoseturn !== 0) {
+//   $('#diff-cell').text('Choose a different cell')
 
 module.exports = {
   playGame
